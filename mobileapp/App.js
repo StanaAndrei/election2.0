ccd73a46e88093a7d0cf3c66a25730aca8a63fc5
StanaAndrei <stadey33@gmail.com>
2023-06-23 17:09:09 +0300

    added native base

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -2,12 +2,13 @@
 import * as React from 'react';
 import HomeStack from './routes/HomeStack';
 import AuthStack from './routes/AuthStack';
+import { NativeBaseProvider } from 'native-base';
 
 function App() {
     if (1) {
-        return <>
-            <AuthStack/>
-        </>
+        return <NativeBaseProvider>
+        <AuthStack />
+     </NativeBaseProvider>
     }
     return <>
         <HomeStack />
--- mobileapp/screens/LoginScreen/LoginScreen.jsx
+++ mobileapp/screens/LoginScreen/LoginScreen.jsx
@@ -1,13 +1,30 @@
-import { View, Text, Button } from 'react-native';
+import { Input, Icon, Stack, Pressable, Button, Box, Text } from 'native-base';
+import React, { useState, useEffect } from 'react';
+import { MaterialIcons } from "@expo/vector-icons";
 
 function LoginScreen(props) {
-    return (
-        <>
-            <View>
-                <Text>Login!</Text>
-            </View>
-        </>
-    );
+    const [show, setShow] = useState(false);
+
+    return <>
+        <Stack space={4} w="100%" alignItems="center" paddingTop={'55%'}>
+            <Input w={{
+                base: "75%",
+                md: "25%"
+            }} InputLeftElement={<Icon as={<MaterialIcons name="person" />} size={5} ml="2" color="muted.400" />} placeholder="Name" />
+            <Input w={{
+                base: "75%",
+                md: "25%"
+            }} type={show ? "text" : "password"} InputRightElement={<Pressable onPress={() => setShow(!show)}>
+                <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" />
+            </Pressable>} placeholder="Password" />
+            <Box alignItems="center">
+            <Button onPress={null} size={'lg'}> 
+                <Text fontSize="lg">login</Text>
+            </Button>
+            </Box>
+        </Stack>
+        
+    </>
 }
 
 export default LoginScreen;
