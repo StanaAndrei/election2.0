91d14d48d86fa04b49764a44b39d15cf1c9caba1
StanaAndrei <stadey33@gmail.com>
2023-07-04 15:26:20 +0300

    changed var->const at index.js

--- mobileapp/api/index.js
+++ mobileapp/api/index.js
@@ -1,8 +1,9 @@
 import axios from 'react-native-axios'
 import { API_URL } from "@env";
+import useAuthRepo from '../repositories/auth.repo';
 
 console.log(API_URL);
-var axiosInst = axios.create({
+const axiosInst = axios.create({
     baseURL: API_URL + '/api/v1',
     timeout: 1000,
     headers: { 'X-Custom-Header': 'foobar' }
