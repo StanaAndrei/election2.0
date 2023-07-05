a423bb6ee0574e4d7f3bbbf9255dcbfc74c7b43c
StanaAndrei <stadey33@gmail.com>
2023-07-05 12:48:13 +0300

    beautify profile

--- mobileapp/screens/ProfileScreen/ProfileScreen.jsx
+++ mobileapp/screens/ProfileScreen/ProfileScreen.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Heading, Text, HStack, Spinner, Button } from 'native-base';
+import { Heading, HStack, Spinner, Button } from 'native-base';
 import useAuthRepo from '../../repositories/auth.repo';
 import UserAPI from '../../api/user.api';
 import jwt_decode from 'jwt-decode';
-import { LogBox } from 'react-native';
+import { SafeAreaView, View, Text, Pressable, Image } from 'react-native';
+import tailwind from 'twrnc';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 function ProfileScreen({ navigation }) {
     const logOut = useAuthRepo(state => state.logOut)
@@ -38,11 +41,33 @@ function ProfileScreen({ navigation }) {
     }
 
     return (
-        <>
-            <Text fontSize="5xl">{user.fullName}</Text>
-            <Button marginLeft={'31%'} w={'34%'} size={'md'} colorScheme={'secondary'} onPress={handleDel}>Delete</Button>
-            <Button marginLeft={'31%'} w={'34%'} onPress={logOut}>logout</Button>
-        </>
+        <SafeAreaView style={tailwind`flex-1 bg-slate-50`}>
+            <View style={tailwind`flex-1 items-center justify-center gap-8`}>
+                <Image
+                    source={{ uri: 'https://source.unsplash.com/random' }}
+                    style={tailwind`w-24 h-24 rounded-full`}
+                    resizeMode="cover"
+                />
+                <View style={tailwind`gap-2 items-center`}>
+                    <Text style={tailwind`text-slate-900 text-3xl font-bold`}>{user.fullName}</Text>
+                    <Text style={tailwind`text-slate-900 text-lg`}>{user.email}</Text>
+                </View>
+            </View>
+            <View style={tailwind`flex-1 justify-center gap-8`}>
+                <Pressable style={tailwind`flex-row items-center gap-2 px-8`}>
+                    <Ionicons name="settings-outline" size={24} style={tailwind`text-slate-900`} />
+                    <Text style={tailwind`text-slate-900 text-lg`}>Settings</Text>
+                </Pressable>
+                <Pressable style={tailwind`flex-row items-center gap-2 px-8`}>
+                    <Ionicons name="help-buoy-outline" size={24} style={tailwind`text-slate-900`} />
+                    <Text style={tailwind`text-slate-900 text-lg`}>Help</Text>
+                </Pressable>
+                <Pressable onPress={logOut} style={tailwind`flex-row items-center gap-2 px-8`}>
+                    <MaterialIcons name="logout" size={24} style={tailwind`text-slate-900`} />
+                    <Text style={tailwind`text-slate-900 text-lg`}>Logout</Text>
+                </Pressable>
+            </View>
+        </SafeAreaView>
     );
 }
 
