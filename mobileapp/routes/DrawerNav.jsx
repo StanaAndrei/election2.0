29ab9bca37148e6f2df822a5a7424e1058331c5b
StanaAndrei <stadey33@gmail.com>
2023-07-09 10:40:43 +0300

    added toggle theme btn

--- mobileapp/routes/DrawerNav.jsx
+++ mobileapp/routes/DrawerNav.jsx
@@ -1,30 +1,52 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Button, View,  } from 'react-native';
+import { DrawerContentScrollView, createDrawerNavigator, DrawerItem, DrawerItemList,  } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from './HomeStack';
 import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
 import ProfileStack from './ProfileStack';
+import { Box, useColorMode, useColorModeValue, Text } from 'native-base';
 
 
 function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>lalalal</Text>
-    </View>
-  );
+    return (
+        <Box flex={1} bg={'coolGray.700'} width={'100%'} mt={1} safeArea >
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>lalalal</Text>
+            </View>
+        </Box>
+    );
 }
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNav() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="HomeStack" component={HomeStack} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-        <Drawer.Screen name='ProfileStack' component={ProfileStack} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+    const { toggleColorMode } = useColorMode();
+
+    const text = useColorModeValue('Light', 'Dark');
+    const bg = useColorModeValue('wormGrey.50', 'coolGrey.700')
+
+    const handleToggleTheme = () => {
+        toggleColorMode();
+        console.log('====================================');
+        console.log(bg);
+        console.log('====================================');
+    }
+
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home" drawerContent={props => {
+                return (
+                    <DrawerContentScrollView {...props}>
+                        <DrawerItemList {...props} />
+                        <DrawerItem label={`toggle: ${text}`} onPress={handleToggleTheme} />
+                    </DrawerContentScrollView>
+                )
+            }}>
+                <Drawer.Screen name="HomeStack" component={HomeStack} />
+                <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+                <Drawer.Screen name='ProfileStack' component={ProfileStack} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
