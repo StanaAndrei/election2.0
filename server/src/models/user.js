fd61ac419acbe6262e7cbcc80d19d61747179262
StanaAndrei <stadey33@gmail.com>
2023-07-11 14:01:20 +0300

    start asoc

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Pool, { foreignKey: 'userId' })
     }
   }
   User.init({
