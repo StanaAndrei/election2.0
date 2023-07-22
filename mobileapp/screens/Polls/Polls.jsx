be3baf853481ab77c2f9f8941969053d6d37ce02
StanaAndrei <stadey33@gmail.com>
2023-07-22 22:22:19 +0300

    added isfocused subscr at polls

--- mobileapp/screens/Polls/Polls.jsx
+++ mobileapp/screens/Polls/Polls.jsx
@@ -4,32 +4,31 @@ import { Button, Box, Center, } from "native-base";
 import PollAPI from '../../api/poll.api';
 import useAuthRepo from '../../repositories/auth.repo';
 import jwt_decode from 'jwt-decode';
+import { useIsFocused } from '@react-navigation/native';
 
 function Polls({ navigation }) {
     const token = useAuthRepo(state => state.token)
     const [data, setData] = useState(null);
+    const isFocused = useIsFocused();
 
-    const onGoBack = () => {
+    const onGoBack = () => {}
+
+    const fetchPolls = () => {
         PollAPI.getPollsOf(jwt_decode(token).userId).then(res => {
-            console.log(res.data);
             setData(res.data);
         })
     }
 
     useEffect(() => {
-        PollAPI.getPollsOf(jwt_decode(token).userId).then(res => {
-            console.log(res.data);
-            setData(res.data);
-        })
-    }, [])
+        isFocused && fetchPolls();
+    }, [isFocused])
 
     return (
         <>
             <Button onPress={() => navigation.navigate('CreatePollScreen', {onGoBack})}>new poll</Button>
             <Text>{data?.firstName}'s polls:</Text>
             {
-                data?.polls.map((elem, id) => {
-                    console.log(elem.name);
+                data?.polls.reverse().map((elem, id) => {
                     return <Box width="100%" bg="primary.500" p="4" shadow={2} _text={{
                         fontSize: "md",
                         fontWeight: "bold",
