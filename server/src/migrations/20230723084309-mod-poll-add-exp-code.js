2c4afe9527c75f983ed66ee480fcf2254a236761
StanaAndrei <stadey33@gmail.com>
2023-07-23 20:17:39 +0300

    added expdate&code migs

---
+++ server/src/migrations/20230723084309-mod-poll-add-exp-code.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {/*
+    await queryInterface.addColumn('polls', 'code', {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+    })
+    await queryInterface.addColumn('polls', 'expdate', {
+        type: Sequelize.DATE,
+        allowNull: false
+    })//*/
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('polls', 'code')
+    await queryInterface.removeColumn('polls', 'expdate')//*/
+  }
+};
